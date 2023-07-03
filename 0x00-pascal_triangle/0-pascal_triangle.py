9202c92141e4cb18fa180ea96f996178c88d72a4
chrisdanielwise <chrisdanielwise@gmail.com>
2023-07-03 16:06:28 +0100

    Added a README file

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,14 +1,29 @@
+#!/usr/bin/python3
+""" Module for Pascal's triangle """
+
+
 def pascal_triangle(n):
+    """ Returns list of lists of ints representing Pascal’s triangle of n """
+
     if n <= 0:
         return []
 
-    triangle = [[1]]
-    while len(triangle) < n:
-        previous_row = triangle[-1]
-        current_row = [1]
-        for i in range(len(previous_row) - 1):
-            current_row.append(previous_row[i] + previous_row[i + 1])
-        current_row.append(1)
-        triangle.append(current_row)
+    pascal = [[1]]
+    if n == 1:
+        return pascal
 
-    return triangle
+    for i in range(1, n):
+        left = -1
+        right = 0
+        in_pas = []
+        for j in range(i+1):
+            num = 0
+            if left > -1:
+                num += pascal[i - 1][left]
+            if right < i:
+                num += pascal[i - 1][right]
+            left += 1
+            right += 1
+            in_pas.append(num)
+        pascal.append(in_pas)
+    return pascal
